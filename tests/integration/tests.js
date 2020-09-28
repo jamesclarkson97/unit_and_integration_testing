1d8c84b0e5effdf3ec8c4faa9a7010872adf6738
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-09-28 22:34:45 +0100

    can divide to 0 and get error

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -63,6 +63,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('500000');
   })
 
+  it('should return an error message when divided by 0', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('Error');
+  })
+
   
 
 });
